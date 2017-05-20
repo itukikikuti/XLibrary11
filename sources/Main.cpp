ad772a380a76d20ed3ce58eb008a2e964c83d1e2
Naoki Nakagawa <xxvraqwa@gmail.com>
2017-05-20 19:40:09 +0900

    三角のポリゴンを描画

--- sources/Main.cpp
+++ sources/Main.cpp
@@ -6,6 +6,11 @@
 #pragma comment(lib, "d3d9.lib")
 #pragma comment(lib, "d3dx9.lib")
 
+struct Vertex {
+	float x, y, z;
+	D3DCOLOR color;
+};
+
 bool CreateDevice(LPDIRECT3D9 direct3D, LPDIRECT3DDEVICE9 *device, HWND windowHandle, D3DPRESENT_PARAMETERS *presentParameter, D3DDEVTYPE deviceType, DWORD behaviourFlag) {
 	if (SUCCEEDED(direct3D->CreateDevice(D3DADAPTER_DEFAULT, deviceType, windowHandle, behaviourFlag, presentParameter, device))) {
 		return true;
@@ -79,8 +84,27 @@ int __stdcall WinMain(HINSTANCE instanceHandle, HINSTANCE, LPTSTR, int showComma
 		}
 	}
 
+	device->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
+	device->SetRenderState(D3DRS_LIGHTING, false);
+	device->SetRenderState(D3DRS_ZENABLE, D3DZB_TRUE);
+	device->SetRenderState(D3DRS_ZWRITEENABLE, true);
+	device->SetFVF(D3DFVF_XYZ | D3DFVF_DIFFUSE);
+
 	ShowWindow(windowHandle, showCommand);
 
+	Vertex vertex[6] = {
+		{ -1.0f, 0.0f, 0.0f, D3DCOLOR_XRGB(255, 0, 0) },
+		{ 1.0f, 0.0f, 0.0f, D3DCOLOR_XRGB(0, 255, 0) },
+		{ 0.0f, 1.0f, 0.0f, D3DCOLOR_XRGB(0, 0, 255) },
+	};
+	D3DXMATRIX worldMatrix;
+	D3DXMATRIX viewMatrix;
+	D3DXMATRIX projectionMatrix;
+	D3DXMatrixIdentity(&worldMatrix);
+	D3DXMatrixIdentity(&viewMatrix);
+	D3DXMatrixIdentity(&projectionMatrix);
+	
+	int frame = 0;
 	MSG windowMessage = {};
 	while (windowMessage.message != WM_QUIT) {
 		Sleep(1);
@@ -89,8 +113,20 @@ int __stdcall WinMain(HINSTANCE instanceHandle, HINSTANCE, LPTSTR, int showComma
 			DispatchMessage(&windowMessage);
 		}
 		else {
+			frame++;
 			device->Clear(0, nullptr, D3DCLEAR_TARGET | D3DCLEAR_STENCIL | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(128, 192, 255), 1.0f, 0);
 			device->BeginScene();
+
+			D3DXMatrixRotationY(&worldMatrix, frame * 0.05f);
+			device->SetTransform(D3DTS_WORLD, &worldMatrix);
+
+			D3DXMatrixLookAtLH(&viewMatrix, &D3DXVECTOR3(0.0f, 1.0f, 5.0f), &D3DXVECTOR3(0.0f, 0.0f, 0.0f), &D3DXVECTOR3(0.0f, 1.0f, 0.0f));
+			device->SetTransform(D3DTS_VIEW, &viewMatrix);
+
+			D3DXMatrixPerspectiveFovLH(&projectionMatrix, D3DXToRadian(60), windowWidth / (float)windowHeight, 0.0f, 100.0f);
+			device->SetTransform(D3DTS_PROJECTION, &projectionMatrix);
+
+			device->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 1, vertex, sizeof(Vertex));
 
 			device->EndScene();
 			device->Present(nullptr, nullptr, nullptr, nullptr);
