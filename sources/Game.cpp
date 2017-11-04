9542c1a72476c6b0194ba193f4feea5906a69881
中川 <xxvraqwa@gmail.com>
2017-11-04 18:23:18 +0900

    デフォルトでAlphaブレンドするように

--- sources/Game.cpp
+++ sources/Game.cpp
@@ -190,6 +190,24 @@ ID3D11DeviceContext& Game::GetDeviceContext() {
 		GetDevice().CreateInputLayout(inputElementDesc, inputElementDescCount, vertexShaderBlob->GetBufferPointer(), vertexShaderBlob->GetBufferSize(), inputLayout.GetAddressOf());
 		vertexShaderBlob->Release();
 		deviceContext->IASetInputLayout(inputLayout.Get());
+
+		ID3D11BlendState* blendState = nullptr;
+		D3D11_BLEND_DESC blendDesc;
+		blendDesc.AlphaToCoverageEnable = false;
+		blendDesc.IndependentBlendEnable = false;
+		blendDesc.RenderTarget[0].BlendEnable = true;
+		blendDesc.RenderTarget[0].SrcBlend = D3D11_BLEND_SRC_ALPHA;
+		blendDesc.RenderTarget[0].DestBlend = D3D11_BLEND_INV_SRC_ALPHA;
+		blendDesc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
+		blendDesc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;
+		blendDesc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ZERO;
+		blendDesc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
+		blendDesc.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
+
+		float blendFactor[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
+		GetDevice().CreateBlendState(&blendDesc, &blendState);
+		deviceContext->OMSetBlendState(blendState, blendFactor, 0xffffffff);
+		blendState->Release();
 	}
 
 	return *deviceContext.Get();
--- sources/Main.cpp
+++ sources/Main.cpp
@@ -10,14 +10,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPTSTR, int) {
 
 	Game::SetTitle("‚­‚Ÿw‚¹drftgy‚Ó‚¶‚±lp");
 
-	Sprite sprite1(L"box.jpg");
+	Sprite sprite1(L"clock.png");
 	Sprite sprite2(L"4.JPG");
 
 	float t = 0.0f;
 	while (Game::Update()) {
 		t += Game::GetDeltaTime();
 
-		sprite1.Draw(200.0f, 200.0f, t * 360.0f, 1.0f);
+		sprite1.Draw(200.0f, 200.0f, t * (360.0f), 1.3f);
+		sprite1.Draw(200.0f, 200.0f, t * (360.0f / 60.0f), 1.0f);
+		sprite1.Draw(200.0f, 200.0f, t * (360.0f / 60.0f / 12.0f), 0.7f);
+
 		sprite2.Draw(1000.0f, 250.0f, 0.0f, 1.0f);
 	}
 
