2fc489a33c81d16c25536c06f1c9cfb74fa0fa6f
中川 <xxvraqwa@gmail.com>
2017-11-10 17:36:14 +0900

    ビルトインのシェーダで使っていたstringクラスをchar*に変更

--- sources/Game.h
+++ sources/Game.h
@@ -309,14 +309,14 @@ namespace GameLibrary {
 			ID3DBlob *errorBlob = nullptr;
 
 			if (path == nullptr) {
-				std::string source("\
+				char* shader = "\
 cbuffer CB:register(b0){matrix W;matrix V;matrix P;float4 C;};\
 Texture2D Tex:register(t0);SamplerState S:register(s0);\
 struct VO{float4 pos:SV_POSITION;float4 c:COLOR;float2 uv:TEXCOORD;};\
 VO VS(float4 v:POSITION,float2 uv:TEXCOORD){VO o=(VO)0;o.pos=mul(W,v);o.pos=mul(V,o.pos);o.pos=mul(P,o.pos);o.c=C;o.uv=uv;return o;}\
-float4 PS(VO o):SV_TARGET{return Tex.Sample(S,o.uv)*o.c;}");
+float4 PS(VO o):SV_TARGET{return Tex.Sample(S,o.uv)*o.c;}";
 
-				D3DCompile(source.c_str(), source.size(), nullptr, nullptr, nullptr, entryPoint, shaderModel, shaderFlags, 0, out, &errorBlob);
+				D3DCompile(shader, strlen(shader), nullptr, nullptr, nullptr, entryPoint, shaderModel, shaderFlags, 0, out, &errorBlob);
 			}
 			else {
 				D3DCompileFromFile(GetWideChar(path), nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, entryPoint, shaderModel, shaderFlags, 0, out, &errorBlob);
