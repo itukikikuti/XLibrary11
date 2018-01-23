41fa59ade522c9113c7c8bc5de9ade5119af7cca
中川 <xxvraqwa@gmail.com>
2018-01-23 17:18:53 +0900

    Timeクラスを作成

--- sources/App.h
+++ sources/App.h
@@ -33,63 +33,64 @@ public:
 #include "Window.h"
 #include "Screen.h"
 #include "Input.h"
+#include "Time.h"
 	PUBLIC App() = delete;
 	PUBLIC static HWND GetWindowHandle() {
-		return GetWindow().GetHandle();
+		return GetWindowInstance().GetHandle();
 	}
 	PUBLIC static DirectX::XMINT2 GetWindowSize() {
-		return GetWindow().GetSize();
+		return GetWindowInstance().GetSize();
 	}
 	PUBLIC static void SetWindowSize(int width, int height) {
-		GetWindow().SetSize(width, height);
+		GetWindowInstance().SetSize(width, height);
 	}
 	PUBLIC static wchar_t* GetTitle() {
-		return GetWindow().GetTitle();
+		return GetWindowInstance().GetTitle();
 	}
 	PUBLIC static void SetTitle(const wchar_t* title) {
-		GetWindow().SetTitle(title);
+		GetWindowInstance().SetTitle(title);
 	}
 	PUBLIC static void SetFullScreen(bool isFullscreen) {
-		GetWindow().SetFullScreen(isFullscreen);
+		GetWindowInstance().SetFullScreen(isFullscreen);
 	}
 	PUBLIC static ID3D11Device& GetDevice() {
-		return GetScreen().GetDevice();
+		return GetScreenInstance().GetDevice();
 	}
 	PUBLIC static IDXGISwapChain& GetSwapChain() {
-		return GetScreen().GetSwapChain();
+		return GetScreenInstance().GetSwapChain();
 	}
 	PUBLIC static ID3D11DeviceContext& GetContext() {
-		return GetScreen().GetContext();
+		return GetScreenInstance().GetContext();
 	}
 	PUBLIC static ID3D11RenderTargetView& GetRenderTargetView(bool isResize = false) {
-		return GetScreen().GetRenderTargetView();
+		return GetScreenInstance().GetRenderTargetView();
+	}
+	PUBLIC static bool GetKey(int VK_CODE) {
+		return GetInputInstance().GetKey(VK_CODE);
+	}
+	PUBLIC static bool GetKeyUp(int VK_CODE) {
+		return GetInputInstance().GetKeyUp(VK_CODE);
+	}
+	PUBLIC static bool GetKeyDown(int VK_CODE) {
+		return GetInputInstance().GetKeyDown(VK_CODE);
+	}
+	PUBLIC static DirectX::XMINT2 GetMousePosition() {
+		return GetInputInstance().GetMousePosition();
+	}
+	PUBLIC static float GetTime() {
+		return GetTimeInstance().GetTime();
+	}
+	PUBLIC static float GetDeltaTime() {
+		return GetTimeInstance().GetDeltaTime();
+	}
+	PUBLIC static int GetFrameRate() {
+		return GetTimeInstance().GetFrameRate();
 	}
 	PUBLIC static DirectX::XMMATRIX GetViewMatrix() {
 		return DirectX::XMMatrixLookAtLH(DirectX::XMVectorSet(App::GetWindowSize().x / 2.0f, -App::GetWindowSize().y / 2.0f, 0.0f, 0.0f), DirectX::XMVectorSet(App::GetWindowSize().x / 2.0f, -App::GetWindowSize().y / 2.0f, 1.0f, 0.0f), DirectX::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f));
 	}
 	PUBLIC static DirectX::XMMATRIX GetProjectionMatrix() {
 		return DirectX::XMMatrixOrthographicLH(App::GetWindowSize().x * 1.0f, App::GetWindowSize().y * 1.0f, -1.0f, 1.0f);
-	}
-	PUBLIC static DirectX::XMINT2 GetMousePosition() {
-		return GetInput().GetMousePosition();
-	}
-	PUBLIC static bool GetKey(int VK_CODE) {
-		return GetInput().GetKey(VK_CODE);
-	}
-	PUBLIC static bool GetKeyUp(int VK_CODE) {
-		return GetInput().GetKeyUp(VK_CODE);
-	}
-	PUBLIC static bool GetKeyDown(int VK_CODE) {
-		return GetInput().GetKeyDown(VK_CODE);
-	}
-	PUBLIC static float GetTime() {
-		return Time();
-	}
-	PUBLIC static float GetDeltaTime() {
-		return DeltaTime();
-	}
-	PUBLIC static int GetFrameRate() {
-		return FrameRate();
 	}
 	PUBLIC static void AddFont(const wchar_t* filePath) {
 		AddFontResourceExW(filePath, FR_PRIVATE, nullptr);
@@ -103,59 +104,12 @@ public:
 			return false;
 		}
 
-		GetInput().Update();
-		PrecessDeltaTime();
-		PrecessTime();
-		ProcessFrameRate();
+		GetInputInstance().Update();
+		GetTimeInstance().Update();
 
 		GetContext().ClearRenderTargetView(&GetRenderTargetView(), color);
 
 		return true;
-	}
-	PRIVATE static float& Time() {
-		static float time = 0.0f;
-		return time;
-	}
-	PRIVATE static float& DeltaTime() {
-		static float deltaTime = 0.0f;
-		return deltaTime;
-	}
-	PRIVATE static int& FrameRate() {
-		static int frameRate = 0;
-		return frameRate;
-	}
-	PRIVATE static LARGE_INTEGER GetCounter() {
-		LARGE_INTEGER counter;
-		QueryPerformanceCounter(&counter);
-		return counter;
-	}
-	PRIVATE static LARGE_INTEGER GetCountFrequency() {
-		LARGE_INTEGER frequency;
-		QueryPerformanceFrequency(&frequency);
-		return frequency;
-	}
-	PRIVATE static void PrecessTime() {
-		Time() += GetDeltaTime();
-	}
-	PRIVATE static void PrecessDeltaTime() {
-		static LARGE_INTEGER preTime = GetCounter();
-		static LARGE_INTEGER frequency = GetCountFrequency();
-
-		LARGE_INTEGER time = GetCounter();
-		DeltaTime() = (float)(time.QuadPart - preTime.QuadPart) / frequency.QuadPart;
-		preTime = GetCounter();
-	}
-	PRIVATE static void ProcessFrameRate() {
-		static float time = 0.0f;
-		static int c = 0;
-
-		c++;
-		time += GetDeltaTime();
-		if (time >= 1.0f) {
-			FrameRate() = c;
-			c = 0;
-			time -= 1.0f;
-		}
 	}
 	PRIVATE static bool ProcessMessage() {
 		static MSG message = {};
@@ -172,17 +126,21 @@ public:
 
 		return false;
 	}
-	PRIVATE static Window& GetWindow() {
+	PRIVATE static Window& GetWindowInstance() {
 		static std::unique_ptr<Window> window(new Window());
 		return *window.get();
 	}
-	PRIVATE static Screen& GetScreen() {
+	PRIVATE static Screen& GetScreenInstance() {
 		static std::unique_ptr<Screen> screen(new Screen());
 		return *screen.get();
 	}
-	PRIVATE static Input& GetInput() {
+	PRIVATE static Input& GetInputInstance() {
 		static std::unique_ptr<Input> input(new Input());
 		return *input.get();
+	}
+	PRIVATE static Time& GetTimeInstance() {
+		static std::unique_ptr<Time> time(new Time());
+		return *time.get();
 	}
 };
 
---
+++ sources/Time.h
@@ -0,0 +1,50 @@
+class Time {
+	PRIVATE float time = 0.0f;
+	PRIVATE float deltaTime = 0.0f;
+	PRIVATE int frameRate = 0;
+	PRIVATE float second = 0.0f;
+	PRIVATE int frameCount = 0;
+	LARGE_INTEGER preCount;
+	LARGE_INTEGER frequency;
+
+	PUBLIC Time() {
+		preCount = GetCounter();
+		frequency = GetCountFrequency();
+	}
+	PUBLIC float GetTime() {
+		return time;
+	}
+	PUBLIC float GetDeltaTime() {
+		return deltaTime;
+	}
+	PUBLIC int GetFrameRate() {
+		return frameRate;
+	}
+	PUBLIC void Update() {
+
+		LARGE_INTEGER count = GetCounter();
+		deltaTime = (float)(count.QuadPart - preCount.QuadPart) / frequency.QuadPart;
+		preCount = GetCounter();
+
+		time += deltaTime;
+
+
+		frameCount++;
+		second += deltaTime;
+		if (second >= 1.0f) {
+			frameRate = frameCount;
+			frameCount = 0;
+			second -= 1.0f;
+		}
+	}
+	PRIVATE LARGE_INTEGER GetCounter() {
+		LARGE_INTEGER counter;
+		QueryPerformanceCounter(&counter);
+		return counter;
+	}
+	PRIVATE LARGE_INTEGER GetCountFrequency() {
+		LARGE_INTEGER frequency;
+		QueryPerformanceFrequency(&frequency);
+		return frequency;
+	}
+};
