0c6f9acf170a753e4e2fc8c6a9e10088a797d6db
中川 <xxvraqwa@gmail.com>
2018-02-09 14:26:18 +0900

    さっきのコミットし忘れ

--- sources/App.hpp
+++ sources/App.hpp
@@ -22,107 +22,105 @@
 #define PRIVATE private:
 #define PROTECTED protected:
 
-namespace GameLibrary {
-
 #include "Math.hpp"
 
-	class App {
+class App {
 
-	#include "Window.hpp"
-	#include "Graphics.hpp"
-	#include "Input.hpp"
-	#include "Timer.hpp"
+#include "Window.hpp"
+#include "Graphics.hpp"
+#include "Input.hpp"
+#include "Timer.hpp"
 
-		PUBLIC App() = delete;
-		PUBLIC static HWND GetWindowHandle() {
-			return GetWindow().GetHandle();
-		}
-		PUBLIC static std::vector<UINT>& GetWindowMessages() {
-			return GetWindow().GetMessages();
-		}
-		PUBLIC static DirectX::XMFLOAT2 GetWindowSize() {
-			return GetWindow().GetSize();
-		}
-		PUBLIC static void SetWindowSize(float width, float height) {
-			GetWindow().SetSize(width, height);
-		}
-		PUBLIC static wchar_t* GetTitle() {
-			return GetWindow().GetTitle();
-		}
-		PUBLIC static void SetTitle(const wchar_t* title) {
-			GetWindow().SetTitle(title);
-		}
-		PUBLIC static void SetFullScreen(bool isFullscreen) {
-			GetWindow().SetFullScreen(isFullscreen);
-		}
-		PUBLIC static ID3D11Device& GetGraphicsDevice() {
-			return GetGraphics().GetDevice();
-		}
-		PUBLIC static ID3D11DeviceContext& GetGraphicsContext() {
-			return GetGraphics().GetContext();
-		}
-		PUBLIC static IDXGISwapChain& GetGraphicsMemory() {
-			return GetGraphics().GetMemory();
-		}
-		PUBLIC static bool GetKey(int VK_CODE) {
-			return GetInput().GetKey(VK_CODE);
-		}
-		PUBLIC static bool GetKeyUp(int VK_CODE) {
-			return GetInput().GetKeyUp(VK_CODE);
-		}
-		PUBLIC static bool GetKeyDown(int VK_CODE) {
-			return GetInput().GetKeyDown(VK_CODE);
-		}
-		PUBLIC static DirectX::XMFLOAT2 GetMousePosition() {
-			return GetInput().GetMousePosition();
-		}
-		PUBLIC static void SetMousePosition(DirectX::XMFLOAT2 position) {
-			GetInput().SetMousePosition(position.x, position.y);
-		}
-		PUBLIC static void SetMousePosition(float x, float y) {
-			GetInput().SetMousePosition(x, y);
-		}
-		PUBLIC static void SetShowCursor(bool isShowCursor) {
-			GetInput().SetShowCursor(isShowCursor);
-		}
-		PUBLIC static float GetTime() {
-			return GetTimer().GetTime();
-		}
-		PUBLIC static float GetDeltaTime() {
-			return GetTimer().GetDeltaTime();
-		}
-		PUBLIC static int GetFrameRate() {
-			return GetTimer().GetFrameRate();
-		}
-		PUBLIC static void AddFont(const wchar_t* filePath) {
-			AddFontResourceExW(filePath, FR_PRIVATE, nullptr);
-		}
-		PUBLIC static bool Refresh() {
-			GetGraphicsMemory().Present(1, 0);
+	PUBLIC App() = delete;
+	PUBLIC static HWND GetWindowHandle() {
+		return GetWindow().GetHandle();
+	}
+	PUBLIC static std::vector<UINT>& GetWindowMessages() {
+		return GetWindow().GetMessages();
+	}
+	PUBLIC static DirectX::XMFLOAT2 GetWindowSize() {
+		return GetWindow().GetSize();
+	}
+	PUBLIC static void SetWindowSize(float width, float height) {
+		GetWindow().SetSize(width, height);
+	}
+	PUBLIC static wchar_t* GetTitle() {
+		return GetWindow().GetTitle();
+	}
+	PUBLIC static void SetTitle(const wchar_t* title) {
+		GetWindow().SetTitle(title);
+	}
+	PUBLIC static void SetFullScreen(bool isFullscreen) {
+		GetWindow().SetFullScreen(isFullscreen);
+	}
+	PUBLIC static ID3D11Device& GetGraphicsDevice() {
+		return GetGraphics().GetDevice();
+	}
+	PUBLIC static ID3D11DeviceContext& GetGraphicsContext() {
+		return GetGraphics().GetContext();
+	}
+	PUBLIC static IDXGISwapChain& GetGraphicsMemory() {
+		return GetGraphics().GetMemory();
+	}
+	PUBLIC static bool GetKey(int VK_CODE) {
+		return GetInput().GetKey(VK_CODE);
+	}
+	PUBLIC static bool GetKeyUp(int VK_CODE) {
+		return GetInput().GetKeyUp(VK_CODE);
+	}
+	PUBLIC static bool GetKeyDown(int VK_CODE) {
+		return GetInput().GetKeyDown(VK_CODE);
+	}
+	PUBLIC static DirectX::XMFLOAT2 GetMousePosition() {
+		return GetInput().GetMousePosition();
+	}
+	PUBLIC static void SetMousePosition(DirectX::XMFLOAT2 position) {
+		GetInput().SetMousePosition(position.x, position.y);
+	}
+	PUBLIC static void SetMousePosition(float x, float y) {
+		GetInput().SetMousePosition(x, y);
+	}
+	PUBLIC static void SetShowCursor(bool isShowCursor) {
+		GetInput().SetShowCursor(isShowCursor);
+	}
+	PUBLIC static float GetTime() {
+		return GetTimer().GetTime();
+	}
+	PUBLIC static float GetDeltaTime() {
+		return GetTimer().GetDeltaTime();
+	}
+	PUBLIC static int GetFrameRate() {
+		return GetTimer().GetFrameRate();
+	}
+	PUBLIC static void AddFont(const wchar_t* filePath) {
+		AddFontResourceExW(filePath, FR_PRIVATE, nullptr);
+	}
+	PUBLIC static bool Refresh() {
+		GetGraphicsMemory().Present(1, 0);
 
-			if (!GetWindow().Update()) return false;
-			GetInput().Update();
-			GetTimer().Update();
+		if (!GetWindow().Update()) return false;
+		GetInput().Update();
+		GetTimer().Update();
 
-			return true;
-		}
-		PRIVATE static Window& GetWindow() {
-			static std::unique_ptr<Window> window(new Window());
-			return *window.get();
-		}
-		PRIVATE static Graphics& GetGraphics() {
-			static std::unique_ptr<Graphics> graphics(new Graphics());
-			return *graphics.get();
-		}
-		PRIVATE static Input& GetInput() {
-			static std::unique_ptr<Input> input(new Input());
-			return *input.get();
-		}
-		PRIVATE static Timer& GetTimer() {
-			static std::unique_ptr<Timer> timer(new Timer());
-			return *timer.get();
-		}
-	};
+		return true;
+	}
+	PRIVATE static Window& GetWindow() {
+		static std::unique_ptr<Window> window(new Window());
+		return *window.get();
+	}
+	PRIVATE static Graphics& GetGraphics() {
+		static std::unique_ptr<Graphics> graphics(new Graphics());
+		return *graphics.get();
+	}
+	PRIVATE static Input& GetInput() {
+		static std::unique_ptr<Input> input(new Input());
+		return *input.get();
+	}
+	PRIVATE static Timer& GetTimer() {
+		static std::unique_ptr<Timer> timer(new Timer());
+		return *timer.get();
+	}
+};
 
 #include "Texture.hpp"
 #include "Material.hpp"
@@ -130,4 +128,3 @@ namespace GameLibrary {
 #include "Mesh.hpp"
 #include "Sprite.hpp"
 #include "Text.hpp"
-}
