8973a00cdddab2979b5741702a70889e75c02e14
中川 <xxvraqwa@gmail.com>
2018-06-11 23:31:06 +0900

    App.hpp to Library.hpp

--- sources/Main.cpp
+++ sources/Main.cpp
@@ -1,6 +1,6 @@
 ﻿#define XLIBRARY_NAMESPACE_BEGIN
 #define XLIBRARY_NAMESPACE_END
-#include "App.hpp"
+#include "Library.hpp"
 #include "Library.cpp"
 #include <sstream>
 
@@ -8,7 +8,7 @@ using namespace std;
 
 int MAIN()
 {
-    Library::Generate(L"sources/App.hpp", L"XLibrary11.hpp");
+    Library::Generate(L"sources/Library.hpp", L"XLibrary11.hpp");
 
     Camera camera;
     camera.position = Float3(0.0f, 1.0f, -5.0f);
